7e9865869388195c3da5787456a879101e76eb70
GoodGuyzaxx <drawww111@gmail.com>
2022-11-17 14:06:51 +0900

    Fixing some function

--- app/src/main/java/id/idkr/diceroller/MainActivity.kt
+++ app/src/main/java/id/idkr/diceroller/MainActivity.kt
@@ -8,23 +8,38 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    //Toast notif when button clicked.
-    private fun rollDice(){
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUP: Button = findViewById(R.id.count_up)
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..6).random()
-        val countUpValue = 1
-        resultText.text= "1"
-        countUP.setOnClickListener{resultText.text=randomInt.toString()}
         rollButton.setOnClickListener{rollDice()}
 
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener{countUp()}
     }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if (resultText.text == "Hello World!"){
+            resultText.text = "1"
+        }else{
+
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+    }
+
 }
